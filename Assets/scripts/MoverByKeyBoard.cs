e32fd7a3cb46ad04578647a8ffcd1bb01a9ba64b
doviec <doviechitiz@gmail.com>
2020-11-03 11:45:31 +0200

    added boundries FINAL

--- Assets/scripts/MoverByKeyBoard.cs
+++ Assets/scripts/MoverByKeyBoard.cs
@@ -3,44 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /**
- * 
+ * This component allows us to control the movment of the object with the keyboard arrows
  */
 
 public class MoverByKeyBoard : MonoBehaviour
 {
+    //step represents the distance of the movment
+    [SerializeField]
     float step = 0.01f;
     float scaleX;
     float scaleY;
     void Start()
     {
+        //the current position of this object
         scaleX = transform.position.x;
         scaleY = transform.position.y;
     }
 
     void Update()
     {
+        //getkey function allows movment when the key is being pressed for a while
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += new Vector3(0, step, 0);
-            Debug.Log("up");
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position -= new Vector3(0, step, 0);
-            Debug.Log("down");
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(step, 0, 0);
-            Debug.Log("right");
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position -= new Vector3(step, 0, 0);
-            Debug.Log("left");
             
         }
-        
+       
     }
 }
 
---
+++ Assets/scripts/boundries.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component gives boundries to the object we will return the object to the center
+ * of the map as soon as he reaches his limit.
+ */
+public class boundries : MonoBehaviour
+{
+    float scaleX;
+    float scaleY;
+    float rightLimit = 6.3f;
+    float leftLimit = -8.4f;
+    float topLimit = 5.5f;
+    float bottomLimit = -6.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleX = transform.position.x;
+        scaleY = transform.position.y;
+        if (scaleX > rightLimit )
+        {
+            transform.position = new Vector3(rightLimit, scaleY, 0);
+        }
+        if ( scaleX < leftLimit)
+        {
+            transform.position = new Vector3(leftLimit, scaleY, 0);
+        }
+        if (scaleY > topLimit)
+        {
+            transform.position = new Vector3(scaleX, topLimit, 0);
+        }
+        if (scaleY < bottomLimit)
+        {
+            transform.position = new Vector3(scaleX, bottomLimit, 0);
+        }
+    }
+}
